f48b5e4c563d9ff2e765ce64bee07897581f76cb
silenc3502 <gcccompil3r@gmail.com>
2018-10-26 17:04:51 +0900

    Fix ROI

--- ocv_proj/third.cpp
+++ ocv_proj/third.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
@@ -12,16 +13,28 @@ int main(int argc, char **argv)
 	int h = img.rows;
 	int w = img.cols;
 
+	printf("h = %d, w = %d\n", h, w);
+
 	if(img.empty())
 		return -1;
 
-	Mat roi = img(Range(310, 539), Range(120, 475));
+	printf("h - 310 = %d, w - 539 = %d, h - 120 = %d, w - 475 = %d\n",
+			h - 310, w - 539, h - 120, w - 475);
 
+	//Mat roi = img(Range(h - 310, w - 539), Range(h - 120, w - 475));
+	//Mat roi = img(Range(310, 539), Range(120, 475));
+	//Mat roi = img(Range(310, 475), Range(539, 539));
+	//Mat roi = img(Range(539, 800), Range(310, 120));
+	//Mat roi = img(Range(310, 120), Range(539, 800));
+	Mat roi = img(Range(310, 539), Range(120, 800));
+
+	imshow("Original Image", img);
 	imshow("ROI Image", roi);
 
 	waitKey(0);
 
 	destroyWindow("ROI Image");
+	destroyWindow("Original Image");
 
 	return 0;
 }
